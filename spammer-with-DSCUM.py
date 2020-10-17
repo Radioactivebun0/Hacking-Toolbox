eb44a6719e826e2fd40563d6bbcae7e5b2459e73
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2020-10-16 22:17:18 -0400

    Update spammer-with-DSCUM.py

--- spammer-with-DSCUM.py
+++ spammer-with-DSCUM.py
@@ -1,7 +1,7 @@
 import discum
 
 def login():
-    bot = discum.Client(token='NzY2NzU1NTM5MTIzMzA2NTQ4.X4n_Fg.g4UrMo7JSY4tDFak7ekIrle9jmU')
+    bot = discum.Client(token='Your token here')
     test()
 
 def test():
